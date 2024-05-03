dd24a286c97433632a6fb32716d764e97fb98a4c
zivmi <zivmi@github.com>
2024-05-03 19:07:08 +0200

    Add T-dependent P-mash. Save config.json of each run. Add docstrings. Cleanup and lint.

--- models/voc_simulation.py
+++ models/voc_simulation.py
@@ -1,21 +1,34 @@
-#!/home/miroslav/miniforge3/envs/voc python
-
+import json
+import time
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Ridge
-import os
 from sklearn import set_config
-set_config(assume_finite=True) # up to 10% speedup
-import time
-import json
+set_config(assume_finite=True)  # up to 10% speedup
+
 
 def make_rff(G, P, gamma=2, seed=59148, output_type='numpy'):
+    """
+    Generate P Random Fourier Features from the set of independent variables G.
+
+    Parameters:
+        G: 2d numpy array of independent variables, shape (n_samples, n_features)
+        where n_samples is the number of timestamps 
+        P: number of RFFs to generate
+        gamma: scaling factor in the sin and cos functions 
+        seed: random seed 
+        output_type: 'numpy' or 'pandas'
+
+    Returns:
+        S: matrix of random features, the shape of S is (len(G), P)
+    """
     np.random.seed(seed)
     omegas = np.random.normal(0, 1, (15, int(P/2)))
     A = gamma * G @ omegas
-    S_sin = np.sin(A) 
+    S_sin = np.sin(A)
     S_cos = np.cos(A)
-    S = np.full((S_sin.shape[0], S_sin.shape[1] + S_cos.shape[1]), fill_value=np.nan, dtype=S_sin.dtype)
+    S = np.full((S_sin.shape[0], S_sin.shape[1] +
+                S_cos.shape[1]), fill_value=np.nan, dtype=S_sin.dtype)
     S[:, 0::2] = S_sin
     S[:, 1::2] = S_cos
 
@@ -26,103 +39,166 @@ def make_rff(G, P, gamma=2, seed=59148, output_type='numpy'):
     else:
         raise ValueError('output_type must be "pandas" or "numpy"')
 
+
 def single_run(run_inputs, run_params, delta_t=1):
     """
-    Run the backtest for a single run.
-    S: matrix of features
-    R: vector of target variable, shifted once 
-    T_list: list of training window lengths
-    model_list: list of models to train (ridge regressions with different lambdas)
-    P_list: list of number of features to use for each model
-    delta_t: time step for retraining the model
+    The main function for running a simulation for a fixed seed. It is wrapped by the
+    run_simulation function, where inputs and parameters are handled. This function 
+    contains the calculations.
+
+    Parameters:
+        run_inputs: tuple of inputs S (RFFs), and returns R already shifted once
+        run_params: tuple of parameters T_list, P_dict, model_dict. model_dict is 
+            a dictionary of models for each T in T_list. Each model is a list of Ridge
+            regressions with different lambdas. Lambda is determined by T*z, where z is
+            a parameter that is tracked by the index of the model in the list. P_dict contains
+            lists of number of features to use for each T. 
+        delta_t: time step for retraining the model. For example, if delta_t=1, the model
+            is retrained at each time step. If delta_t=10, the model is retrained every 10
+            time steps.
 
     Returns:
+        beta_norm_sq: array of **squared** L2 norm of the coefficients of the model
+        return_forecasts: array of return forecasts
+        strategy_returns: array of strategy returns. Strategy returns are the product of
+            return forecasts and actual returns at each time step, or in other words the
+            trading strategy takes position in the index equal to the return forecast.
     """
-
+    # unpack inputs and parameters
     S, R = run_inputs
-    T_list, P_list, model_dict = run_params
-
-    # initialize arrays for storing results
+    T_list, P_dict, model_dict = run_params
+    min_T = min(T_list)  # usually =12
     num_of_models = len(model_dict[T_list[0]])
+    num_of_Ps = len(P_dict["12"])
+    tmax = len(S)
 
-    min_T = min(T_list) # usually =12
-
-    # dimensions: (ts, Ts, Ps, lambdas)
-    output_shape = (len(S)-min_T, len(T_list), len(P_list), num_of_models)
-    beta_norm_sq = np.full(shape=output_shape, 
-                    fill_value=np.nan, 
-                    dtype=np.float64)
-    return_forecasts = np.full(shape=output_shape, 
-                               fill_value=np.nan, 
+    # initialize arrays for storing results. Dimensions: (ts, Ts, Ps, lambdas)
+    output_shape = (tmax-min_T, len(T_list), num_of_Ps, num_of_models)
+    beta_norm_sq = np.full(shape=output_shape,
+                           fill_value=np.nan,
+                           dtype=np.float64)
+    return_forecasts = np.full(shape=output_shape,
+                               fill_value=np.nan,
                                dtype=np.float64)
-    strategy_returns = np.full(shape=output_shape, 
-                               fill_value=np.nan, 
+    strategy_returns = np.full(shape=output_shape,
+                               fill_value=np.nan,
                                dtype=np.float64)
 
-    # initialize arrays for storing intermediate variables
-    # training_std = np.full((len(T_arr), S.shape[1]), fill_value=np.nan, dtype=np.float64)
-
+    # helper functions for calculating standardization
     def my_std(x):
-        return np.sqrt(np.sum(np.square(x - x.mean(axis=0)))/len(x))
+        return np.sqrt(np.sum(np.square(x - x.mean(axis=0)))/(len(x)-1))
 
     def standardize(t):
-        training_sets = []
+        """
+        Standardize training sets (one for each window T) and test set of len=1,
+        at time t. It takes S as implicit input.
+        """
+        sets = []
 
         for T in T_list:
-
-            # this take a lot of time to compute
-            # train_std = trainX.std(axis=0) 
-            # train_std = np.sqrt(np.sum((trainX - train_mean)**2, axis=0)/T) # this is equivalent to trainX.std(axis=0)
-            if t-T < 1:
-                training_sets.append((np.nan, np.nan))
+            if t <= T:
+                sets.append((np.nan, np.nan))
             else:
                 training_std = my_std(S[t-T:t])
                 trainX = S[t-T:t] / training_std
                 forecastX = S[t] / training_std
-                training_sets.append((trainX, forecastX))
+                sets.append((trainX, forecastX))
 
-        return training_sets
+        return sets
 
-    grid = [(P_index, model_index) 
-            for P_index in range(len(P_list)) 
-            for model_index in range(num_of_models)]
-    
-
-    for t in range(min_T, len(S), delta_t):
+    for t in range(min_T, tmax, delta_t):
         # print progress
-        if t%100==0:
-            print(f"progress: {t/(len(S)-min_T):2.1%}")
+        if t % 100 == 0:
+            print(f"progress: {t/(tmax-min_T):2.1%}")
 
-        # one standardization for all models, all complexities
-        training_sets = standardize(t)
+        # perform one standardization for all Ts
+        standardized_sets = standardize(t)
 
-        for P_index, model_index in grid:
-            for T_index, T in enumerate(T_list):
-                if t-T < 1:
-                    continue
-                else: 
-                    P = P_list[P_index]
-                    # get model of appropriate shrinkage lambda=T*z, z is tracked by model_index
-                    model = model_dict[T][model_index]
-                    
-                    trainX, forecastX = training_sets[T_index]
-                    trainY = R[t-T:t]
-                    # take first P features for training
-                    model.fit(trainX[:,:P], trainY) 
-                    forecastY = model.predict(forecastX[:P].reshape(1,-1))
+        for T_index, T in enumerate(T_list):
+            if t <= T:
+                continue
 
-                    # store results 
-                    beta_norm_sq[t-min_T, T_index, P_index, model_index] = np.sum(np.square(model.coef_))
-                    return_forecasts[t-min_T, T_index, P_index, model_index] = forecastY[0]
-                    strategy_returns[t-min_T, T_index, P_index, model_index] = forecastY[0] * R[t]
+            trainX, forecastX = standardized_sets[T_index]
+            trainY = R[t-T:t] # already shifted
+
+            P_list = P_dict[str(T)] # json keys must be strings
+            model_list = model_dict[T]
+            grid = [(P_index, model_index)
+                for P_index in range(num_of_Ps)
+                for model_index in range(num_of_models)]
+
+            for P_index, model_index in grid:
+                P = P_list[P_index]
+                # get model of appropriate shrinkage lambda=T*z, z is tracked by model_index
+                # the value of z is equal to z_list[model_index]
+                model = model_list[model_index]
+
+                # train models using the first P features from S matrix
+                model.fit(trainX[:, :P], trainY)
+                forecastY = model.predict(forecastX[:P].reshape(1, -1))
+
+                # store results
+                beta_norm_sq[t-min_T, T_index, P_index,
+                                model_index] = np.sum(np.square(model.coef_))
+                return_forecasts[t-min_T, T_index,
+                                    P_index, model_index] = forecastY[0]
+                strategy_returns[t-min_T, T_index, P_index,
+                                    model_index] = forecastY[0] * R[t]
 
     return beta_norm_sq, return_forecasts, strategy_returns
 
-def run_simulation(seed):
+
+def run_simulation(seed, path_to_processed, path_to_outputs):
+    """
+    Run a single simulation for a fixed seed. This function controls IO operations
+    and calls the single_run function where the calculations are done.
+
+    The function requires the following folder structure:
+
+    path_to_processed
+        └── processed_data.csv
+    
+    path_to_outputs
+        ├── beta_norm_sq/
+        ├── return_forecasts/
+        ├── strategy_returns/
+        ├── configs/
+        └── config.json
+
+    Parameters:
+        seed: random seed
+        path_to_processed: path to the folder with the preprocessed data
+        path_to_outputs: path to the folder interim outputs and config.json are stored
+
+    Returns:
+        None. The function writes the results to the files in the specified folders.
+    """
     print(f"seed: {seed}")
+    data = pd.read_csv(
+            path_to_processed+"/processed_data.csv", index_col=0, parse_dates=True)
+
+    # load parameters from config file
+    with open(path_to_outputs+"/config.json", 'r') as fp:
+        config = json.load(fp)
+        gamma = config['gamma']
+        T_list = config['T_list']
+        z_list = config['z_list']
+        P_dict = config['P_dict']
+        P_max = config['P_max']
+        delta_t = config['delta_t']
+        solver = config['solver']
+        fit_intercept = config['fit_intercept']
+
+    # create dictionary of models for each T in T_list
+    model_dict = {T: [Ridge(alpha=T*z, fit_intercept=fit_intercept, solver=solver) 
+                        for z in z_list] for T in T_list}
+
+    # inputs
+    G = data.iloc[:, :-1].values # remove last column which is the target variable
+    R = data.iloc[:, -1].values  # target variable, already shifted in prereprocessing
     S = make_rff(G, P_max, gamma=gamma, seed=seed, output_type='numpy')
     run_inputs = (S, R)
-    run_params = (T_list, P_list, model_dict)
+    run_params = (T_list, P_dict, model_dict)
 
     st = time.time()
 
@@ -133,81 +209,37 @@ def run_simulation(seed):
     print('Execution time:', elapsed_time, 'seconds')
 
     # write numpy arrays to files
-    np.save(path_to_data+f"/interim/simulation_outputs/beta_norm_sq/{seed:04}beta_norm_sq.npy", b)
-    np.save(path_to_data+f"/interim/simulation_outputs/return_forecasts/{seed:04}return_forecasts.npy", r)
-    np.save(path_to_data+f"/interim/simulation_outputs/strategy_returns/{seed:04}strategy_returns.npy", sr)
+    np.save(path_to_outputs +
+            f"/beta_norm_sq/{seed:04}beta_norm_sq.npy", b)
+    np.save(path_to_outputs +
+            f"/return_forecasts/{seed:04}return_forecasts.npy", r)
+    np.save(path_to_outputs+
+            f"/strategy_returns/{seed:04}strategy_returns.npy", sr)
+    
+    # save separate config file for each seed
+    config['seed'] = seed
+    with open(path_to_outputs+f"/configs/{seed:04}config.json", 'w') as fp:
+        json.dump(config, fp)
 
-    # write configuration to file
+    # update last_run_seed in the config file
+    config['last_run_seed'] = seed
 
-    #config['last_run_seed'] = seed
-    config['time_elapsed'] = elapsed_time
-
-    with open(path_to_data+"/interim/simulation_outputs/config.json", 'w') as fp:
+    # update general config file
+    with open(path_to_outputs+"/config.json", 'w') as fp:
         json.dump(config, fp)
 
     return None
 
 
 if __name__ == '__main__':
-    path_to_data = "data"
-    data = pd.read_csv(path_to_data+"/processed/processed_data.csv", index_col=0, parse_dates=True)
+    path_to_processed = "data/processed" # specify this
+    path_to_outputs = "data/interim/final_simulation_outputs" # specify this
 
-    # load parameters from config file
-    with open(path_to_data+"/interim/simulation_outputs/config.json", 'r') as fp:
+    # load the last and the max seeds from the config file
+    with open(path_to_outputs+"/config.json", 'r') as fp:
         config = json.load(fp)
         last_run_seed = config['last_run_seed']
-        gamma = config['gamma']
-        T_list = config['T_list']
-        z_list = config['z_list']
-        P_list = config['P_list']
-        P_max = max(P_list)
-        delta_t = config['delta_t']
         max_seed = config['max_seed']
-        model_dict = {T: [Ridge(alpha=np.square(T*z), fit_intercept=True) for z in z_list] for T in T_list}
-        # print the solver used in the ridge regression
-        print(f"Solver used in ridge regression: {model_dict[T_list[0]][0].solver}")
-
-    # inputs
-    G = data.iloc[:,:-1].values # remove last column which is the target variable
-    R = data.iloc[:,-1].values # target variable, it is already shifted
-
-    paralellize = False
-
-    # number of threads to used
-    num_threads = 3
-
-    if paralellize:
-        from multiprocessing import Pool
-
-        for seed in range(last_run_seed + 1, max_seed + 1, num_threads):
-            with Pool(num_threads) as p:
-                # take num_threads seeds from last_run_seed+1 to last_run_seed+num_threads and run simulations in parallel
-                p.map(run_simulation, [seed + i for i in range(num_threads)])
-
-                # check if all simulations are done and updated the last_run_seed in the config file
-                # Check if all simulations are done
-                completed_simulations = range(seed, seed + num_threads)
-                all_simulations_done = all(os.path.exists(f"{path_to_data}/interim/simulation_outputs/strategy_returns/{seed:04}strategy_returns.npy") for seed in completed_simulations)
-
-                # Update the last_run_seed in the config file
-                if all_simulations_done:
-                    config['last_run_seed'] = last_run_seed + num_threads
-                    with open(path_to_data+"/interim/simulation_outputs/config.json", 'w') as fp:
-                        json.dump(config, fp)
-                else: 
-                    print('Not all simulations are done')
-                    print('Seeds not done:', [seed for seed in completed_simulations if not os.path.exists(f"{path_to_data}/interim/simulation_outputs/strategy_returns/{seed:04}strategy_returns.npy")])
-
-                    # if not all simulations are done, update the last_run_seed in the config file to the minimal seed that is not done
-                    config['last_run_seed'] = min(seed for seed in completed_simulations if not os.path.exists(f"{path_to_data}/interim/simulation_outputs/strategy_returns/{seed:04}strategy_returns.npy"))
-                    with open(path_to_data+"/interim/simulation_outputs/config.json", 'w') as fp:
-                        json.dump(config, fp)
-
-                print('All simulations done for seeds:', all_simulations_done)
     
-    else:
-        for seed in range(last_run_seed + 1, max_seed + 1):
-            run_simulation(seed)
-            config['last_run_seed'] = seed            
-            with open(path_to_data+"/interim/simulation_outputs/config.json", 'w') as fp:
-                json.dump(config, fp)
+    for seed in range(last_run_seed + 1, max_seed + 1):
+        run_simulation(seed, path_to_processed, path_to_outputs)
